78ddc22ee494d6020b29463bd6cb677ffa3ffc22
jmburke1 <51562013+jmburke1@users.noreply.github.com>
2026-05-22 00:58:59 -0400

    Adding coverage of the dictionary editor view and the sanitizer view

---
+++ src/test/java/promptsanitizer/view/DictionaryEditorViewTest.java
@@ -0,0 +1,26 @@
+package promptsanitizer.view;
+
+import org.junit.jupiter.api.Test;
+import org.mockito.MockedConstruction;
+import org.mockito.Mockito;
+import promptsanitizer.controller.DictionaryEditorController;
+import promptsanitizer.controller.SanitizerController;
+import promptsanitizer.model.DictionaryModel;
+import promptsanitizer.model.SanitizerModel;
+
+import javax.swing.*;
+
+public class DictionaryEditorViewTest {
+    @Test
+    void createUIMakesTheFrameVisible() {
+        try(MockedConstruction<JFrame> jfMC = Mockito.mockConstruction(JFrame.class)) {
+            DictionaryEditorView dictionaryEditorView = new DictionaryEditorView(
+                    "/path/to/file.json",
+                    Mockito.mock(DictionaryEditorController.class),
+                    Mockito.mock(DictionaryModel.class)
+            );
+            dictionaryEditorView.createUI();
+            Mockito.verify(jfMC.constructed().get(0)).setVisible(true);
+        }
+    }
+}
---
+++ src/test/java/promptsanitizer/view/SanitizerViewTest.java
@@ -0,0 +1,24 @@
+package promptsanitizer.view;
+
+import org.junit.jupiter.api.Test;
+import org.mockito.MockedConstruction;
+import org.mockito.Mockito;
+import promptsanitizer.controller.SanitizerController;
+import promptsanitizer.model.SanitizerModel;
+
+import javax.swing.*;
+
+public class SanitizerViewTest {
+    @Test
+    void createUIMakesTheFrameVisible() {
+        try(MockedConstruction<JFrame> jfMC = Mockito.mockConstruction(JFrame.class)) {
+            SanitizerView sanitizerView = new SanitizerView(
+                    "/path/to/file.json",
+                    Mockito.mock(SanitizerController.class),
+                    Mockito.mock(SanitizerModel.class)
+            );
+            sanitizerView.createUI();
+            Mockito.verify(jfMC.constructed().get(0)).setVisible(true);
+        }
+    }
+}
