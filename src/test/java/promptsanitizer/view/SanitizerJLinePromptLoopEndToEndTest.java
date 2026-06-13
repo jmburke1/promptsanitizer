2c5c0a5e274c579927934393f8b385398ee4c737
jmburke1 <51562013+jmburke1@users.noreply.github.com>
2026-06-13 15:32:43 -0400

    Still not ready for this to be enabled yet.  I definitely have an idea for it, for next commit.

--- src/test/java/promptsanitizer/view/SanitizerJLinePromptLoopEndToEndTest.java
+++ src/test/java/promptsanitizer/view/SanitizerJLinePromptLoopEndToEndTest.java
@@ -28,6 +28,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 
+@Disabled
 class SanitizerJLinePromptLoopEndToEndTest {
 
     private ByteArrayOutputStream capturedOutput;
